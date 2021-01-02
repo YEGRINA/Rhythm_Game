8817dd8e207a36366890b02bfc8a338623d0b62a
YEGRINA <64945012+YEGRINA@users.noreply.github.com>
2021-01-03 01:10:43 +0900

    set intro background Main.java
    게임 시작 배경 설정

---
+++ DoomChit/Main.java
@@ -0,0 +1,15 @@
+package doomchit_2;
+
+public class Main {
+
+	// 해상도 설정
+	public static final int SCREEN_WIDTH = 1452;
+	public static final int SCREEN_HEIGHT = 817;
+	
+	public static void main(String[] args) {
+		
+		new DoomChit();
+
+	}
+
+}
