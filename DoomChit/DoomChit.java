aea6c59283a3253598ba00a63b40c85789ea8e07
YEGRINA <64945012+YEGRINA@users.noreply.github.com>
2021-01-03 01:12:26 +0900

    create frame set intro background DoomChit.java
    프레임 생성 및 게임 시작 배경 설정

---
+++ DoomChit/DoomChit.java
@@ -0,0 +1,40 @@
+package doomchit_2;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class DoomChit extends JFrame {
+
+	private Image screenImage;
+	private Graphics screenGraphic;
+	
+	private Image introBackground;
+	
+	public DoomChit() {
+		setTitle("Doom Chit");
+		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		setResizable(false);  // 사이즈 변경 x
+		setLocationRelativeTo(null);  // 화면 중앙에 위치
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		
+		introBackground = new ImageIcon(Main.class.getResource("../images/introBackground(title).png")).getImage();
+			
+	}
+	
+	public void paint(Graphics g) {
+		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
+		screenGraphic = screenImage.getGraphics();
+		screenDraw(screenGraphic);
+		g.drawImage(screenImage, 0, 0, null);
+	}
+	
+	public void screenDraw(Graphics g) {
+		g.drawImage(introBackground, 0, 0, null);
+		this.repaint();
+	}
+	
+}
