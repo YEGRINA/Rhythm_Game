ffa3671c586cbf5283c4e0f15949f767414e8e55
YEGRINA <64945012+YEGRINA@users.noreply.github.com>
2021-01-09 19:55:46 +0900

    Create menuBar, exitButton, add exitMusic
    메뉴바 구현
    exit 버튼, 음악 삽입

--- DoomChit/DoomChit.java
+++ DoomChit/DoomChit.java
@@ -1,42 +1,107 @@
-package doom_chit_3;
+package doom_chit_4;
 
+import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class DoomChit extends JFrame {
 
 	private Image screenImage;
 	private Graphics screenGraphic;
+
+	private Image introBackground = new ImageIcon(Main.class.getResource("../images/introBackground(title).png"))
+			.getImage();
+	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menuBar.png")));
+
+	private ImageIcon exitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/exitBasic.png"));
+	private ImageIcon exitButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/exitEntered.png"));
 	
-	private Image introBackground;
+	private JButton exitButton = new JButton(exitButtonBasicImage);
+	
+	private int mouseX, mouseY;
 	
 	public DoomChit() {
+		setUndecorated(true);
 		setTitle("Doom Chit");
 		setSize(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
-		setResizable(false);  // 사이즈 변경 x
-		setLocationRelativeTo(null);  // 화면 중앙에 위치
+		setResizable(false); // 사이즈 변경 x
+		setLocationRelativeTo(null); // 화면 중앙에 위치
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		setBackground(new Color(0, 0, 0, 0));
+		setLayout(null);
+
+		exitButton.setBounds(1407, 0, 40, 40);
+		exitButton.setBorderPainted(false);
+		exitButton.setContentAreaFilled(false);
+		exitButton.setFocusPainted(false);
+		exitButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				exitButton.setIcon(exitButtonEnteredImage);
+				exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+				Music buttonEnteredMusic = new Music("buttonEnteredMusic.mp3", false);
+				buttonEnteredMusic.start();
+			}
+			public void mouseExited(MouseEvent e) {
+				exitButton.setIcon(exitButtonBasicImage);
+				exitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+			public void mousePressed(MouseEvent e) {
+				Music buttonPressedMusic = new Music("buttonPressedMusic.mp3", false);
+				buttonPressedMusic.start();
+				try {  // 버튼을 누르자마자 바로 종료되므로 sleep(1000)
+					Thread.sleep(1000);
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+				}
+				System.exit(0);
+			}
+		});
+		add(exitButton);
 		
-		introBackground = new ImageIcon(Main.class.getResource("../images/introBackground(title).png")).getImage();
+		menuBar.setBounds(0, 0, 1452, 40);
+		menuBar.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				mouseX = e.getX();
+				mouseY = e.getY();
+			}
+		});
+		menuBar.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				int x = e.getXOnScreen();
+				int y = e.getYOnScreen();
+				setLocation(x - mouseX, y - mouseY);  // 프레임 위치 설정
+			}
+		});
+		add(menuBar);
 		
 		Music introMusic = new Music("introMusic.mp3", true);
 		introMusic.start();
 	}
-	
+
 	public void paint(Graphics g) {
 		screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 		screenGraphic = screenImage.getGraphics();
 		screenDraw(screenGraphic);
 		g.drawImage(screenImage, 0, 0, null);
 	}
-	
+
 	public void screenDraw(Graphics g) {
 		g.drawImage(introBackground, 0, 0, null);
+		paintComponents(g);
 		this.repaint();
 	}
-	
+
 }
