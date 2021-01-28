128d79c323450fb6d805805039beba13919dc349
YEGRINA <64945012+YEGRINA@users.noreply.github.com>
2021-01-28 22:37:55 +0900

    Add constant for note

--- DoomChit/Main.java
+++ DoomChit/Main.java
@@ -1,10 +1,12 @@
-package doom_chit_5;
+package doom_chit_13;
 
 public class Main {
 
 	// 해상도 설정
 	public static final int SCREEN_WIDTH = 1280;
 	public static final int SCREEN_HEIGHT = 720;
+	public static final int NOTE_SPEED = 7;  // 떨어지는 속도
+	public static final int SLEEP_TIME = 10;  // 떨어지는 주기
 	
 	public static void main(String[] args) {
 		
