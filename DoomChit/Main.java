c2371d5a53eecac375110edae775125c8ce70234
YEGRINA <64945012+YEGRINA@users.noreply.github.com>
2021-01-29 17:19:12 +0900

    NOTE_SPEED up

--- DoomChit/Main.java
+++ DoomChit/Main.java
@@ -1,11 +1,11 @@
-package doom_chit_14;
+package doom_chit_15;
 
 public class Main {
 
 	// 해상도 설정
 	public static final int SCREEN_WIDTH = 1280;
 	public static final int SCREEN_HEIGHT = 720;
-	public static final int NOTE_SPEED = 6;  // 떨어지는 속도
+	public static final int NOTE_SPEED = 7;  // 떨어지는 속도
 	public static final int SLEEP_TIME = 10;  // 떨어지는 주기
 	public static final int REACH_TIME = 1;  // 판정라인에 도달하기까지 시간
 	
